3cd21807f17a1899a59a39254bce0770cc15e882
R. Diaz de Leon <leon@elinter.net>
2013-08-01 18:34:25 -0500

    Cleaned idevices schema

--- models/idevice.rb
+++ models/idevice.rb
@@ -1,24 +1,27 @@
 require 'sequel'
 
-create_table('devices') do
-    primary_key :id
-    String      :token
-    String      :secret
-    String      :version
-end
+def create_tables(db)
 
-create_table('spaces') do
-    primary_key :id
-    String      :name
-    String      :url
-end
+    db.create_table :devices do
+        primary_key :id
+        String      :token
+        String      :secret
+        String      :version
+    end
 
-create_table('space_status') do
-    primary_key :id
-    TrueClass   :status
-    DateTime    :created
-    foreign_key :space_id, :spaces
-    index       :created
-end
+    db.create_table :spaces do
+        primary_key :id
+        String      :name
+        String      :url
+    end
 
-create_join_table(:device_id => :devices, :space_id => :spaces)
+    db.create_table :space_status do
+        primary_key :id
+        TrueClass   :status
+        DateTime    :created
+        foreign_key :space_id, :spaces
+        index       :created
+    end
+
+    db.create_join_table(:device_id => :devices, :space_id => :spaces)
+end

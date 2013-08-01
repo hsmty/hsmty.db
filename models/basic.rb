29f6af14e72d1760382200c8d8c49ddd2dd9db4e
R. Diaz de Leon <leon@elinter.net>
2013-08-01 18:25:03 -0500

    Improved functions for use with load

--- models/basic.rb
+++ models/basic.rb
@@ -1,26 +1,28 @@
 require 'sequel'
 
-create_table(:users) do
-    primary_key :id
-    String      :name
-    String      :last_name
-    String      :email
-    String      :nick
-    String      :password
-end
+def create_tables(db)
+    db.create_table(:users) do
+        primary_key :id
+        String      :name
+        String      :last_name
+        String      :email
+        String      :nick
+        String      :password
+    end
 
-create_table(:roles) do
-    primary_key :id
-    String      :name
-end
+    db.create_table(:roles) do
+        primary_key :id
+        String      :name
+    end
 
-create_join_table(:user_id=>:users, :role_id=>:roles)
-    
-create_table(:happenings) do
-    primary_key :id
-    String      :name
-    DateTime    :date
-    Numeric     :cost
-    DateTime    :created
-    foreign_key :created_by, :users
+    db.create_join_table(:user_id=>:users, :role_id=>:roles)
+        
+    db.create_table(:happenings) do
+        primary_key :id
+        String      :name
+        DateTime    :date
+        Numeric     :cost
+        DateTime    :created
+        foreign_key :created_by, :users
+    end
 end
